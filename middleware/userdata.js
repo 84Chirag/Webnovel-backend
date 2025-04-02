9f6bb1c0feffc1c60795eb8f14a39fd19b20da4a
Chirag gour <chiragvgour@gmail.com>
2025-04-02 11:15:57 +0530

    Update userdata.js

--- middleware/userdata.js
+++ middleware/userdata.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET
-// const JWT_SECRET = "chirag-gour";
 
 const userdata = (req, res, next) => {
     const token = req.header('auth-token');
